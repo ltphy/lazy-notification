a7d472d2a3929b5e6049241b174e0eca0517292a
Phy Lieng <phylieng97@gmail.com>
2022-05-15 13:59:50 +0700

    feature<notification> add bottom tabs package

---
+++ src/StackNavigation/route.constants.ts
@@ -0,0 +1,14 @@
+enum RouteName {
+    HomeScreen = "HomeScreen",
+    NotificationScreen = "NotificationScreen",
+    ProfileScreen = "ProfileScreen"
+}
+
+export type RouteStackParamList = {
+    HomeScreen: undefined,
+    NotificationScreen: undefined;
+    ProfileScreen: undefined;
+}
+
+
+export {RouteName};
