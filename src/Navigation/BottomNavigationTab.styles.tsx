bf91a0c71a75749722294f3218302f4ee5c6b608
Phy Lieng <phylieng97@gmail.com>
2022-05-15 21:50:59 +0700

    feature<notification> add bottom navigation icon tab

---
+++ src/Navigation/BottomNavigationTab.styles.tsx
@@ -0,0 +1,15 @@
+import {StyleSheet} from "react-native";
+
+
+const bottomNavigationTabsStyles = StyleSheet.create({
+    tabBar: {
+        height: 60,
+        // paddingBottom: 5,
+    },
+    tabBarLabel: {
+        fontSize: 12,
+        paddingBottom: 10
+    }
+});
+
+export default bottomNavigationTabsStyles;
--- src/Navigation/BottomNavigationTab.tsx
+++ src/Navigation/BottomNavigationTab.tsx
@@ -4,18 +4,44 @@ import ActivityScreen from "../screens/ActivityScreen/ActivityScreen";
 import ProfileScreen from "../screens/ProfileScreen/ProfileScreen";
 import PaymentScreen from "../screens/PaymentScreen/PaymentScreen";
 import React from "react";
-import { NavigationContainer } from '@react-navigation/native';
+import {NavigationContainer} from '@react-navigation/native';
+import Icon from 'react-native-vector-icons/FontAwesome';
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
+import MaterialIcons from 'react-native-vector-icons/MaterialIcons';
+import bottomNavigationTabsStyles from "./BottomNavigationTab.styles";
 
 const Tab = createBottomTabNavigator();
 
 const BottomNavigationTabs = () => {
     return (
         <NavigationContainer>
-            <Tab.Navigator>
-                <Tab.Screen name="Home" component={HomeScreen}/>
-                <Tab.Screen name="ActivityScreen" component={ActivityScreen}/>
-                <Tab.Screen name="ProfileScreen" component={ProfileScreen}/>
-                <Tab.Screen name="Payment" component={PaymentScreen}/>
+            <Tab.Navigator screenOptions={{
+                tabBarActiveTintColor: '#ed7907',
+                tabBarStyle:bottomNavigationTabsStyles.tabBar,
+                tabBarLabelStyle: bottomNavigationTabsStyles.tabBarLabel,
+
+            }}>
+                <Tab.Screen name="Home" component={HomeScreen} options={{
+                    tabBarLabel: 'Home',
+                    tabBarIcon: ({color, size}) => (<MaterialCommunityIcons name="home" color={color} size={size}/>),
+                }}
+
+                />
+                <Tab.Screen name="ActivityScreen" component={ActivityScreen} options={{
+                    tabBarLabel: 'Activity',
+                    tabBarIcon: ({color, size}) => (
+                        <MaterialCommunityIcons name="script-text" color={color} size={size}/>),
+                    tabBarBadge: 2,
+                }}/>
+                <Tab.Screen name="Payment" component={PaymentScreen} options={{
+                    tabBarLabel: 'Payment',
+                    tabBarIcon: ({color, size}) => (<MaterialIcons name="payment" color={color} size={size}/>),
+                }}/>
+                <Tab.Screen name="ProfileScreen" component={ProfileScreen} options={{
+                    tabBarLabel: 'Profile',
+                    tabBarIcon: ({color, size}) => (
+                        <MaterialCommunityIcons name="account-circle-outline" color={color} size={size}/>),
+                }}/>
             </Tab.Navigator>
         </NavigationContainer>
     );
